66ef6f4f57790858ba23c9be8bd2e9e03e602eba
Maxime Bourdin <maxime.bourdin.tic@gmail.com>
2023-05-07 17:53:16 +0200

    update front

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LayoutMainComponent } from './layouts/Main/main.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
-
+  {
+    path: "",
+    component: LayoutMainComponent,
+    children: []
+  },
+  {
+    path: "musicien",
+    component: LayoutMainComponent,
+    loadChildren: () => import("./pages/musicien/musicien.module").then(m => m.MusicienModule),
+  },
 ];
 
 @NgModule({
